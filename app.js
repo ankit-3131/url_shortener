99727373868d63669b054a4a0fb4dfd3fa8cb71b
ankit-3131 <heyyynkit@gmail.com>
2024-12-31 12:34:00 +0530

    WORKING ROUTES ||POSTman CHECKED || CLICKS COUNT

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ const app = express();
 const  PORT = 5000;
 import urlRoute from './router/url.js';
 import connectDB from './controller/configDB.js';'./controller/configDB.js'
+import urlModel from './models/schema.js'
 connectDB();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req,res)=>{
+    try{
+    const shortId = req.params.shortId
+    const event = await urlModel.findOneAndUpdate(
+        {
+            short_id: shortId
+    },
+        {
+            $push:{
+                visitHistory:{
+                    timestamps : Date.now()
+        },
+    },
+}
+    );
+    if (!event) {
+        return res.json({ message: "URL was unknown to us!" });
+    }
+    return res.redirect(event.user_url)}
+
+    catch(error){console.error(error);
+    }
+})
 
 app.listen(PORT, () => {console.log(`Server is running at http://localhost:${PORT}`)});
--- router/url.js
+++ router/url.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const router = express.Router();
 import handlerShortUrlGenerator from '../controller/index.js'
+import urlModel from '../models/schema.js'
 
 router.route('/')
 .get((req,res)=>{
@@ -8,5 +9,26 @@ router.route('/')
 })
 .post(handlerShortUrlGenerator)
 
+router.route('/:shortId')
+.get(async (req,res)=>{
+    try{
+    const shortId = req.params.shortId
+    const data = await urlModel.findOneAndUpdate(
+        {
+        shortId
+    },
+        {
+            $push:{
+                visitHistory:{
+                    timestamps : Date.now()
+        },
+    },
+}
+    );
+    if(!data) return res.json({message: "URL was unknown to us !"});
+    res.redirect(data.user_url);}
+    catch(error){console.error(error);
+    }
+})
 
 export default router
